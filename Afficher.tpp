10858938ecbc70d66f2828346c4fa91efc296b47
SuperQuentin <quentin.aellen@infrack.ch>
2023-01-18 16:36:46 +0100

    feat: add menu display

--- Afficher.tpp
+++ Afficher.tpp
@@ -10,14 +10,24 @@
 // Created by Florian Duruz, Antoine Leresche on 17/01/2023.
 //
 
+#include "Afficher.h"
 #include <iostream>
 
-template<typename T> Afficher<T>::Afficher()
-{
+
+template<typename T>
+Afficher<T>::Afficher() {
 }
 
 template<typename T>
-void Afficher<T>::ajouter_element(T el)
-{
+void Afficher<T>::ajouter_element(T el) {
     elements.push_back(el);
+}
+template<typename T>
+size_t Afficher<T>::afficher_liste() {
+    for (size_t i = 1; auto const &msg: elements) {
+        cout << i << ") : [" << msg->lis_date() << "] - " << msg->lis_source();
+        ++i;
+    }
+
+    return this->elements->size();
 }
--- main.cpp
+++ main.cpp
@@ -2,14 +2,56 @@
 // Fichier principal pour le labo 08 - Messages
 //
 
-#include <iostream>
 #include "Afficher.h"
-#include "emails/Email.h"
 #include "Toots/Toot.h"
+#include "emails/Email.h"
+#include <iostream>
+
+const vector<string> MAIN_MENU_OPTIONS = {
+        "Show Toots",
+        "Show Emails",
+};
+
+const string LEAVE_MSG = "Leave";
+const string ERROR_MENU_ACTION_INPUT = "Sorry this action is not manage, please retry";
+
+void showMenu(const vector<string> &options, const string sep = "> ") {
+    for (size_t i = 0; const auto &option: options) {
+        cout << ++i << sep << option << endl;
+    }
+    cout << 0 << sep << LEAVE_MSG << endl;
+}
+
+void mainMenu() {
+    while (true) {
+        char actionInput = '\0';
+        showMenu(MAIN_MENU_OPTIONS);
+        cin >> actionInput;
+
+        switch (actionInput) {
+            case '0': {
+                return;
+            }
+            case '1': {
+                // TODO: Show Toots
+                break;
+            }
+            case '2': {
+                // TODO: Show Emails
+                break;
+            }
+            default:
+            cerr << ERROR_MENU_ACTION_INPUT << endl;
+        }
+    }
+}
 
 int main() {
     Afficher<Email> emails;
     Email a("emails/email1.txt");
     emails.ajouter_element(a);
+
+    mainMenu();
+
     return 0;
 }
